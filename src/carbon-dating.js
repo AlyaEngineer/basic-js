e1e5f9863fa37d7132c56055c5788f71d365f454
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-01-06 01:45:02 +0100

    feat(carbon-dating): add solution for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,23 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let age = HALF_LIFE_PERIOD / Math.LN2 * Math.log(MODERN_ACTIVITY / sampleActivity);
+  if (typeof sampleActivity === 'string' && !isNaN(Number(sampleActivity)) && sampleActivity > 0 && age > 0) {
+    return Math.ceil(age);
+  }
+  return false;
 }
 
 module.exports = {
